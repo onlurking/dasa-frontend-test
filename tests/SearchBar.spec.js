b8eb2da7032e67fdab13271ac240f5b74a9f2d18
Diogo Felix <diogo2fex@gmail.com>
2019-06-09 21:52:45 -0300

    tests(cypress): move files to tests/e2e

--- tests/SearchBar.spec.js
+++ tests/SearchBar.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Search from '@/components/SearchBar'
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(Search)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
