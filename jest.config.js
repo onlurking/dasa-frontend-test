88488cf21c72bb708fea08de366c51a3d50f5a39
Diogo Felix <diogo2fex@gmail.com>
2019-06-09 21:59:45 -0300

    test(jest): isolate cypress tests from jest

--- jest.config.js
+++ jest.config.js
@@ -5,11 +5,13 @@ module.exports = {
     '^vue$': 'vue/dist/vue.common.js'
   },
   moduleFileExtensions: ['js', 'vue', 'json'],
+  testPathIgnorePatterns: ['<rootDir>/tests/e2e/', '<rootDir>/node_modules/'],
   transform: {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest'
   },
   collectCoverage: true,
+  coverageDirectory: '<rootDir>/tests/unit/coverage',
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue'
